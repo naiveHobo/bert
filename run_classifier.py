b61fc762cf150f6583978513e4d0bcc32d08fa0b
Sarthak Mittal <sarthakmittal2608@gmail.com>
2018-12-20 16:13:26 +0530

    Minor fix

--- run_classifier.py
+++ run_classifier.py
@@ -204,7 +204,7 @@ class MicrosoftProcessor(DataProcessor):
   def get_test_examples(self, data_dir):
     """See base class."""
     return self._create_examples(
-        self._read_tsv(os.path.join(data_dir, "test.tsv"))[:1], "test")
+        self._read_tsv(os.path.join(data_dir, "test.tsv")), "test")
 
   def get_labels(self):
     """See base class."""
